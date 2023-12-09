acd5f522f8891c925c2a7b6e0d8954345a713271
azuki774s <azuki774s@gmail.com>
2023-12-09 13:11:51 +0900

    use-selenium-standalone

--- src/main.py
+++ src/main.py
@@ -23,20 +23,17 @@ lg.addHandler(h)
 
 
 def get_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-dev-shm-usage")
-    UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-    options.add_argument("--user-agent=" + UA)
-    chrome_service = Service(executable_path="/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=chrome_service, options=options)
+    driver = webdriver.Remote(
+        command_executor="http://selenium:4444/wd/hub",
+        options=webdriver.ChromeOptions(),
+    )
     return driver
 
 
 def main():
     lg.info("fetcher start")
+    lg.info("wait sleep for starting server")
+    time.sleep(int(os.getenv("START_SLEEP", default="0")))
     lg.info("Get driver")
     driver = get_driver()
     driver.implicitly_wait(10)
@@ -44,7 +41,6 @@ def main():
     subsession_ids = iracing.get_your_subsession_id(driver)
     for s in subsession_ids:
         iracing.proc_result_record(driver, s)
-
 
 
 if __name__ == "__main__":
