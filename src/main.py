54de39d3dff8932539067c455391286708540230
azuki774s <azuki774s@gmail.com>
2024-03-03 17:20:02 +0900

    update make one-container

--- src/main.py
+++ src/main.py
@@ -22,11 +22,24 @@ h.setFormatter(json_fmt)
 lg.addHandler(h)
 
 
-def get_driver():
+def get_driver_standalone():
     driver = webdriver.Remote(
         command_executor="http://selenium:4444/wd/hub",
         options=webdriver.ChromeOptions(),
     )
+    return driver
+
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    options.add_argument("--user-agent=" + UA)
+    chrome_service = Service(executable_path="/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=chrome_service, options=options)
     return driver
 
 
